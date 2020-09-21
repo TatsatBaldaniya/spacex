93ecbc3df6c508cd7cc3be270b64bd128e0fdd47
Jigar <shahjigar185@gmail.com>
2020-09-21 18:43:31 +0530

    Added more details

--- src/components/IndividualLaunchDetails.js
+++ src/components/IndividualLaunchDetails.js
@@ -23,7 +23,19 @@ export class IndividualLaunchDetails extends Component {
      sendGetRequest();
  }
     render() {    
-        console.log(this.state.launchDetails)
+        const launchDateUnix=this.state.launchDetails.launch_date_unix;
+        //console.log(launchDateUnix);
+        let d=new Date(launchDateUnix*1000).toLocaleString();
+
+        let a=d.split(',');
+        let launch=this.state.launchDetails.launch_success;
+        console.log(launch);
+        if(launch===false){
+            launch='Failure';
+        }else{
+            launch='Success';
+        }
+
         return (
             <div style={{
                 display:this.state.launchDetails.flight_number===undefined?'none':'block'
@@ -32,9 +44,23 @@ export class IndividualLaunchDetails extends Component {
                     textAlign:'center',
                     
                 }}>FlightNumber:{this.state.launchDetails.flight_number}</h1>
+                <p>MissionName:{this.state.launchDetails.mission_name}</p>
+                <p>LaunchYear:{this.state.launchDetails.launch_year}</p>
+                <p style={DateStyle}>LaunchDate:{a[0]}</p>
+                <p style={TimeStyle}>LaunchTime:{a[1]}</p>
+                <p style={{
+                    color:launch==='Failure'?'red':'green'
+                }}>Launch:{launch}</p>
             </div>
-        )
+        ) 
     }
 }
-
+const TimeStyle={
+    color:'yellow',
+    backgroundColor:'gray'
+}
+const DateStyle={
+    color:'red',
+    fontWeight:'bold'
+}
 export default IndividualLaunchDetails
